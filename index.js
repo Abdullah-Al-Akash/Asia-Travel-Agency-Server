a23f516e7c2add13cc2b95c081225ca58c528a26
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-10-30 00:46:10 +0600

    Added Delete Option

--- index.js
+++ index.js
@@ -66,6 +66,20 @@ async function run() {
                         const orders = await cursor.toArray();
                         res.send(orders)
                 })
+
+                // Delete Operation:
+                app.get('/orders/:id', async (req, res) => {
+                        const id = req.params.id;
+                        const query = { _id: ObjectId(id) };
+                        const result = await orderCollection.findOne(query)
+                        res.json(result);
+                })
+                app.delete('/orders/:id', async (req, res) => {
+                        const id = req.params.id;
+                        const query = { _id: ObjectId(id) };
+                        const result = await orderCollection.deleteOne(query)
+                        res.json(result);
+                })
         }
         finally {
                 // client.close();
